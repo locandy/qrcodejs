dc676119e6c929f28091f68396c847bbe3b7cf76
jrwarwick <justin.warwick@gmail.com>
2024-02-28 12:45:32 -0800

    Add the crispEdges render specification for SVG element to avoid occasional pixel border artifact.

--- qrcode.js
+++ qrcode.js
@@ -195,7 +195,7 @@ var QRCode;
 				return el;
 			}
 
-			var svg = makeSVG("svg" , {'viewBox': '0 0 ' + String(nCount) + " " + String(nCount), 'width': '100%', 'height': '100%', 'fill': _htOption.colorLight});
+			var svg = makeSVG("svg" , {'viewBox': '0 0 ' + String(nCount) + " " + String(nCount), 'width': '100%', 'height': '100%', 'fill': _htOption.colorLight, 'shape-rendering': 'crispEdges'});
 			svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
 			_el.appendChild(svg);
 
